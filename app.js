57e26e05a4b613edb7fe0abb4b779e505d6dd761
Jivitesh <70736177+Jivitesh2001@users.noreply.github.com>
2021-05-23 18:16:38 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public")); // for sending static files like css and images
+app.use(express.urlencoded({ extended: true })); //for body-parser
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/signup.html")
+})
+
+app.post("/", function(req, res) {
+    const fName = req.body.firstName;
+    const lName = req.body.lastName;
+    const email = req.body.email;
+    console.log(fName + "\n" + lName + "\n" + email);
+    var data = {
+        members: [{
+            email_address: email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: fName,
+                LNAME: lName
+            }
+        }]
+
+    };
+    const url = 'https://us6.api.mailchimp.com/3.0/lists/6db32c55f2';
+    const options = {
+        method: "post",
+        auth: "jivitesh :13d0c5defd9f8959f82da60e1d039c59-us6"
+    }
+
+
+    const jsonData = JSON.stringify(data);
+    const request = https.request(url, options, function(response) {
+
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html")
+        } else {
+            res.sendFile(__dirname + "/failure.html")
+        }
+
+        response.on("data", function(data) {
+            console.log(JSON.parse(data));
+        });
+
+
+    });
+
+    request.write(jsonData);
+    request.end();
+
+})
+
+app.post("/failure", function(req, res) {
+    res.redirect("/")
+})
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Server is running on port 3000");
+})
+
+//Api key
+//13d0c5defd9f8959f82da60e1d039c59-us6
+
+//List id
+//6db32c55f2
